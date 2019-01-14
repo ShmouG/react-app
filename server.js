a074d1f3d28b041250e1a4a49e8b5fcf6f9544fb
Sam Gross <sammiel.gross@gmail.com>
2019-01-14 15:21:06 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const routes = require('./routes');
+const db = mongoose.connection;
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+}
+
+app.use('/', routes, (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+const databaseUri = 'mongodb://localhost/textadventure'
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+// mongoose.connect(MONGODB_URI || { useNewUrlParser: true });
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI)
+} else {
+  mongoose.connect(databaseUri)
+};
+
+db.once('error', (err) => {
+  console.log('Mongoose error:', err);
+});
+
+db.once('open', () => {
+  console.log('Mongoose connection successful.')
+})
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
